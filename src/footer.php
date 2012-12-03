67096c2e4c81dffa8144fcb08d518a06f94c7d97
Alistair Wooldrige <agw@woolie.co.uk>
2012-12-03 17:31:52 +0000

    Adding initial footer an lightening backround grey

--- src/footer.php
+++ src/footer.php
@@ -7,7 +7,23 @@
             //get_sidebar( 'footer' );
     ?>
 
- </div><!--/.fluid-container-->
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <footer>
+                    <p class="pull-right muted credit">
+                        &copy; 2012 Alistair Wooldrige (OnMyPlate.co.uk)
+                    </p>
+                    <p class="muted credit">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">OnMyPlate.co.uk</a> - Alistair Wooldrige's food blog
+                    </p>
+                </footer>
+            </div>
+        </div>
+    </div>
+
+<!-- /.container-fluid -->
+</div>
 
 <script src="<?php echo get_template_directory_uri(); ?>/jquery/jquery-1.8.3.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/bootstrap/js/bootstrap.min.js"></script>
