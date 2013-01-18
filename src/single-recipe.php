259f8015cc4a068be97fc59c845b40f7bc2a20dc
Alistair Wooldrige <agw@woolie.co.uk>
2013-01-18 16:39:40 +0000

    Single recipes updates
     * Featured post images now link to the full size image
     * Date that the recipe was published is now shown

--- src/single-recipe.php
+++ src/single-recipe.php
@@ -13,12 +13,19 @@
     <div class="row">
         <div class="span8">
             <?php
-            if (has_post_thumbnail( $post->ID )) {
+            if (has_post_thumbnail($post->ID)) {
+                $image_url = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+            ?>
+                <a href="<?php echo $image_url; ?>" target="_blank">
+                <?php
                 echo get_the_post_thumbnail(
                     $post->ID,
                     'omp-recipe-featured-image',
                     array('class' => 'img-polaroid featured-image')
                 );
+                ?>
+                </a>
+            <?php
             }
             ?>
             <div class="lead">
@@ -62,15 +69,20 @@
             );
         }
         ?>
+
+        <h3>Recipe Information</h3>
         <?php
+        $post_date = sprintf(
+            '<time class="entry-date" datetime="%1$s">%2$s</time>',
+            esc_attr(get_the_date('c')),
+            esc_html(get_the_date())
+        );
+
         if($post->recipe_data['Meta'] !== NULL) {
-            echo '<h3>Information</h3>';
             echo ompThemeMeta($post->recipe_data['Meta']);
         }
         ?>
-
-            <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
-
+        <small>Recipe published on <?php echo $post_date; ?>.</small>
         <!-- /span4 -->
         </div>
     <!-- /row -->
