05c6c3bdef4e920a4f63ed922e0426ad43d8ee6b
Alistair Wooldrige <agw@woolie.co.uk>
2013-02-22 17:12:35 +0000

    Moving LiveFyre code right to the bottom of the page

--- src/footer.php
+++ src/footer.php
@@ -20,8 +20,25 @@
 <!-- /.container-fluid -->
 </div>
 
+<?php wp_footer(); ?>
 <script src="<?php echo get_template_directory_uri(); ?>/jquery/jquery-1.8.3.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/bootstrap/js/bootstrap.min.js"></script>
-<?php wp_footer(); ?>
+<script type="text/javascript" src="http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js"></script>
+<script type="text/javascript">
+(function () {
+    var articleId = "<?php the_ID(); ?>";
+    fyre.conv.load({}, [{
+        el: 'livefyre-comments',
+        network: "livefyre.com",
+        siteId: "319302",
+        articleId: articleId,
+        signed: false,
+        collectionMeta: {
+            articleId: articleId,
+            url: fyre.conv.load.makeCollectionUrl(),
+        }
+    }], function() {});
+}());
+</script>
 </body>
 </html>
